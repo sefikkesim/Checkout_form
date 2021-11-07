31260f9558f29c07bc8a1a59804c0b8c53e64c81
sefikkesim <sefikkesim@gmail.com>
2021-11-07 10:14:25 +0200

    Checkout Form

--- app.js
+++ app.js
@@ -2,28 +2,31 @@
 const tax = 0.18;
 const shippingPrice = 15.0;
 
-window.addEventListener("load",()=>{
+window.addEventListener("DOMContentLoaded",()=>{
     localStorage.setItem("tax",tax);
     localStorage.setItem("shippingPrice",shippingPrice)
     calculateCartTotal()
 })
 
-
 let quentityControllerDivs = document.querySelectorAll(".quantity-controller")
+console.log(quentityControllerDivs)
 
 quentityControllerDivs.forEach((quentityControllerDiv)=>{
     let quantityP = quentityControllerDiv.querySelector("#product-quantity");
     quentityControllerDiv.children[0].addEventListener("click",()=>{
         quantityP.textContent = parseInt(quantityP.textContent) -1;
         calculatePrudoctTotal(quantityP);
-        if(quantityP.textContent == "0"){
-            alert("Do you realy want to remove the prudoct from checkout box");
-             quentityControllerDiv.parentElement.parentElement.remove();
-             
+        if(quantityP.textContent <= "0"){
+            quantityP.textContent = "0";
+            quentityControllerDiv.parentElement.parentElement.style.opacity ="0.3"
+            calculatePrudoctTotal(quantityP)
+        }else{
+            
         }
     });
     quentityControllerDiv.children[2].addEventListener("click",()=>{
         quantityP.textContent = parseInt(quantityP.textContent) +1;
+        quentityControllerDiv.parentElement.parentElement.style.opacity ="1"
         calculatePrudoctTotal(quantityP);
     });
     
@@ -32,8 +35,6 @@ quentityControllerDivs.forEach((quentityControllerDiv)=>{
 const calculatePrudoctTotal = (quantityP)=>{
     let productInfoDiv = quantityP.parentElement.parentElement;
     const productPrice = parseFloat(productInfoDiv.querySelector("strong").textContent);
-    console.log(productPrice)
-    console.log(quantityP.textContent)
     let PrudoctTotalPrice = productPrice * parseInt(quantityP.textContent);
     console.log(PrudoctTotalPrice);
     let productTotal = productInfoDiv.querySelector(".product-line-price");
@@ -44,14 +45,12 @@ const calculatePrudoctTotal = (quantityP)=>{
     
 const calculateCartTotal = ()=>{
     let productTotalPrices = document.querySelectorAll(".product-line-price");
-    console.log(productTotalPrices)
     let subtotal = 0;
     productTotalPrices.forEach((productTotalPrice) =>{
         subtotal += parseFloat(productTotalPrice.textContent);
-        console.log(subtotal)
     })
    let cartTax = subtotal * (localStorage.getItem("tax"))
-    let cartshipping = (subtotal > 0 ? shippingPrice : 0);
+    let cartshipping = (subtotal > 150 ? 0 : shippingPrice );
     let cartTotal = cartTax + cartshipping + subtotal;
 
     document.querySelector("#cart-subtotal").children[1].textContent = subtotal.toFixed(2);
@@ -60,5 +59,10 @@ const calculateCartTotal = ()=>{
     document.querySelector("#cart-total").children[1].textContent = cartTotal.toFixed(2);
 }
 
+let removeBtns = document.querySelectorAll(".remove-product")
+removeBtns.forEach((removeBtn)=>{
+removeBtn.addEventListener("click",()=>{
+ removeBtn.parentElement.parentElement.parentElement.remove() })
+})
  
     
